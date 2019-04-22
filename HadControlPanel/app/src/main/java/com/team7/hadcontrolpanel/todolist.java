9925854669e912372a7982aa79873bd5f599f1db
Jiamin Lin <jiamin.lin14@gmail.com>
2019-04-22 12:21:58 -0500

    delete finished

--- HadControlPanel/app/src/main/java/com/team7/hadcontrolpanel/todolist.java
+++ HadControlPanel/app/src/main/java/com/team7/hadcontrolpanel/todolist.java
@@ -28,9 +28,14 @@ public class todolist extends AppCompatActivity implements View.OnClickListener,
     private int itemID = 0;
     int listCount = 0;
     int fbId = 0;
+    String key;
 
     private ArrayList<String> items;
     private ArrayAdapter<String> adapter;
+
+    private ArrayList<String> itemCounter;
+    //private ArrayAdapter<String> adapterCounter;
+
 
     Map <Integer, Integer> test;
 
@@ -43,6 +48,10 @@ public class todolist extends AppCompatActivity implements View.OnClickListener,
         itemsList=findViewById(R.id.items_list);
         items = filehelper.readData( this);
         adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,items );
+
+        itemCounter = filehelper.readData( this);
+        //adapterCounter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,items );
+
         itemsList.setAdapter(adapter);
         btn.setOnClickListener(this );
         itemsList.setOnItemClickListener(this);
@@ -56,12 +65,13 @@ public class todolist extends AppCompatActivity implements View.OnClickListener,
             case R.id.add_btn:
                 String itemEntered = itemET.getText().toString();
                 adapter.add(itemEntered);
+                itemCounter.add(itemEntered);
                 itemET.setText("");
-                listCount = adapter.getCount();
 
                 filehelper.writeData(items,this);
                 db = FirebaseDatabase.getInstance();
                 ref = db.getReference("ToDo List");
+                key = ref.push().getKey();
                 ref.child("Item").child(String.valueOf(itemID)).setValue(itemEntered);
 
                 itemID++;
@@ -70,10 +80,24 @@ public class todolist extends AppCompatActivity implements View.OnClickListener,
                 break;
         }
     }
+    private void deleteItem(String itemId) {
+        DatabaseReference dItem = FirebaseDatabase.getInstance().getReference("ToDo List").child("Item").child(itemId);
+        dItem.removeValue();
+    }
 
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        fbId++;
+        //fbId++;
+        int key = 0;
+        String item = items.get(position);
+        System.out.println(position);
+
+        for(int i=0; i < itemCounter.size(); i++) {
+            if(itemCounter.get(i).contentEquals(item)) {
+                key = i;
+                System.out.println(key);
+            }
+        }
         items.remove(position);
         adapter.notifyDataSetChanged();
         listCount = adapter.getCount();
@@ -81,19 +105,17 @@ public class todolist extends AppCompatActivity implements View.OnClickListener,
         filehelper.writeData(items,this);
         db = FirebaseDatabase.getInstance();
         ref = db.getReference("ToDo List");
-        try {
-            ref.child("Item").child(String.valueOf(position)).removeValue();
-            if(listCount > 1) {
-                reassignKeys(position);
-            }
-            else {
-                test.remove(0);
-                ref.child("Item").child(String.valueOf(0)).removeValue();
-            }
-        } catch(Exception ex) {
-            System.out.println("Nothign to delete");
-        }
-        //Toast.makeText(this, String.valueOf(position), Toast.LENGTH_SHORT).show();
+        //key = ref.child("Item").child(String.valueOf(position)).getKey();
+        //System.out.println(key);
+
+        //deleteItem(key);
+        //String key = items.get(position);
+        System.out.println(key);
+        ref.child("Item").child(String.valueOf(key)).removeValue();
+        //fbId++;
+        //reassignKeys(position);
+
+        Toast.makeText(this, "deleted", Toast.LENGTH_SHORT).show();
     }
 
     public void reassignKeys(int pos) {
