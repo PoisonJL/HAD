76d7c4693bf47f8703276bd8f1404b5e6af84688
Zhizhan Chen <zc002@uark.edu>
2019-04-27 16:54:39 -0500

    tasks

--- HadControlPanel/app/src/main/java/com/team7/hadcontrolpanel/calender.java
+++ HadControlPanel/app/src/main/java/com/team7/hadcontrolpanel/calender.java
@@ -6,6 +6,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.EditText;
@@ -18,15 +19,6 @@ import com.google.firebase.database.FirebaseDatabase;
 
 import android.widget.Toast;
 
-import com.google.firebase.auth.FirebaseAuthException;
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.Button;
-
-
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
@@ -34,28 +26,43 @@ import java.util.Map;
 
 
 public class calender extends AppCompatActivity {
-
-    private EditText title,day,task;
     CalendarView calendarView;
     TextView myDate;
     DatabaseReference databaseReference;
+    private TextInputEditText inputTitle;
+    private  EditText inputDay;
+    private EditText  inputTask ;
+    private FloatingActionButton floatingActionButton1;
+
+
+
+
+
 
     Button btnSave;
-    private FloatingActionButton floadingactionbutton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_calender);
         Button btnSave = (Button) findViewById(R.id.btnSave);
 
-        databaseReference =FirebaseDatabase.getInstance().getReference("tasks");
 
-        title =(EditText)findViewById(R.id.title);
-        task=(EditText)findViewById(R.id.task);
-        day=(EditText)findViewById(R.id.day);
+        databaseReference= FirebaseDatabase.getInstance().getReference("tasks");
+        floatingActionButton1 =findViewById(R.id.floatingActionButton1);
 
+        floatingActionButton1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                startActivity(new Intent(calender.this, DataRetrived.class));
+            }
+
+
+        }
+        ) ;
+        inputTitle =findViewById(R.id.txtTitle);
+        inputDay =(EditText)findViewById(R.id.txtDay);
+        inputTask =(EditText)findViewById(R.id.txtTask);
 
 
 
@@ -63,11 +70,9 @@ public class calender extends AppCompatActivity {
 
         btnSave.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View view) {
                 openDialog();
-//                Toast.makeText(calender.this, "Successfully saved your task, back to main page", Toast.LENGTH_LONG).show();
-//                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-//                startActivity(intent);
+                addTasks();
             }
         });
 
@@ -85,22 +90,52 @@ public class calender extends AppCompatActivity {
             }
         });
     }
-//    public void confirmInput(View view)
-//    {
-//
-//
-//
-//
-//
-//    }
+
+
+
+
 
     public void openDialog() {
         DialogBox dialogBox = new DialogBox();
         dialogBox.show(getSupportFragmentManager(), "Dialog");
     }
 
-    public void confirmInput(View view) {
+
+
+
+public void addTasks(){
+
+
+
+            String titleName = inputTitle.getText().toString();
+             String dayName = inputDay.getText().toString();
+            String  taskName = inputTask.getText().toString();
+
+        if(!TextUtils.isEmpty(titleName)&& !TextUtils.isEmpty((taskName)))
+    {
+
+        String id=databaseReference.push().getKey();
+
+        CalTask tasks =new CalTask( id, taskName,  titleName,  dayName);
+        databaseReference.child(id).setValue(tasks);
+        inputTitle.setText(" ");
+        inputDay.setText(" ");
+        inputTask.setText(" ");
 
 
     }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
 }
