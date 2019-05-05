f2f6455f0b2ef679d77b69fa232b790c4b91f559
benmolina <bsmolina24@gmail.com>
2019-05-05 00:30:12 -0500

    Updated Python program; updated Wi-Fi display
    Wi-Fi display will now show a list of AP's

--- HadControlPanel/app/src/main/java/com/team7/hadcontrolpanel/WifiActivity.java
+++ HadControlPanel/app/src/main/java/com/team7/hadcontrolpanel/WifiActivity.java
@@ -1,15 +1,22 @@
 package com.team7.hadcontrolpanel;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.DialogInterface;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiManager;
 import android.os.AsyncTask;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.google.firebase.database.DatabaseReference;
@@ -21,20 +28,23 @@ import com.jcraft.jsch.JSch;
 import com.jcraft.jsch.Session;
 
 import java.io.ByteArrayOutputStream;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 
 public class WifiActivity extends AppCompatActivity {
-    String wifi, password;
-    EditText wifiInput;
+    String wifi, password, secPro;
+    Spinner wifiInput;
     EditText passInput;
     Button btnConnect;
     private FirebaseDatabase db;
     private DatabaseReference ref;
 
-    // Make the login changes here
-    String username = "jl041";
-    String sshPassword = "Jia0214.";
-    String hostname = "turing.csce.uark.edu";
+    // Pi credentials to connect to the Pi
+    String username = "pi";
+    String sshPassword = "haddevice.";
+    String hostname = "192.168.4.1";
     int port = 22;
 
     @Override
@@ -42,14 +52,63 @@ public class WifiActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wifi);
 
+        // References the Spinner
         wifiInput = findViewById(R.id.wifiInput);
+
+        System.out.println("Now getting Wifi information...");
+
+        // Initializes the class object that allows the app to scan for Wi-Fi networks
+        WifiManager w = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+
+        // Newer versions require this permission, but this permission will only run once
+        // If you experience a glitch, close all Apps (including all Overlay Apps [like Messenger])
+        // and restart the phone
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.ACCESS_FINE_LOCATION }, 1);
+
+        // Get a list of all the Wi-FI networks and store them in nl
+        List<ScanResult> nl = w.getScanResults();
+        List<String> ssidList = new ArrayList<>();
+        System.out.println("Now organizing it into a list...");
+
+        // Go through the list of Wi-Fi networks and add them to the SSID list if they have not been
+        // added already
+        if (nl != null) {
+            for (ScanResult nw : nl) {
+                System.out.println(nw.SSID);
+                String ssidEntry = nw.SSID;
+                String ssidInfo = nw.capabilities;
+                if (ssidInfo.contains("WPA2"))
+                    ssidEntry = ssidEntry + "//" + "WPA2-PSK";
+                else if (ssidInfo.contains("WPA"))
+                    ssidEntry = ssidEntry + "//" + "WPA-PSK";
+                else if (ssidInfo.contains("WEP"))
+                    ssidEntry = ssidEntry + "//" + "WEP-PSK";
+                else
+                    ssidEntry = ssidEntry + "//" + "NONE";
+
+                if (!ssidList.contains(ssidEntry))
+                    ssidList.add(ssidEntry);
+            }
+        }
+
+        // Create an adapter and set the adapter to be the dropdown menu
+        ArrayAdapter<String> wifiAdapt = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, ssidList);
+
+        wifiInput.setAdapter(wifiAdapt);
+
         passInput = findViewById(R.id.passInput);
         btnConnect = findViewById(R.id.btnConnect);
 
+        // When the user clicks the "Connect" button, attempt to connect to the Pi using SSH
         btnConnect.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                wifi = wifiInput.getText().toString();
+
+                wifi = wifiInput.getSelectedItem().toString().split("//")[0];
+                secPro = wifiInput.getSelectedItem().toString().split("//")[1];
+
+                System.out.println(secPro);
                 password = passInput.getText().toString();
                 db = FirebaseDatabase.getInstance();
                 ref = db.getReference("WiFi Credential").push();
@@ -59,21 +118,19 @@ public class WifiActivity extends AppCompatActivity {
                 ref.child("SSID").setValue(wifi);
                 ref.child("PASSWORD").setValue(password);
 
-                wifiInput.setText("");
+                wifiInput.setSelection(-1);
                 passInput.setText("");
 
+                // Required to connect to Pi via SSH
                 new AsyncTask<Integer, Void, Void>() {
                     @SuppressLint("WrongThread")
                     @Override
                     protected Void doInBackground(Integer... integers) {
                         try {
                             // connect to Pi
-                            executeRemoteCommand(username, sshPassword, hostname, port, wifi, password);
-                            //Toast.makeText(MainActivity.this, "You were able to create a file - nice!", Toast.LENGTH_LONG).show();
-
+                            executeRemoteCommand(username, sshPassword, hostname, port, wifi, password, secPro);
                         } catch (Exception e) {
                             e.printStackTrace();
-                            //Toast.makeText(MainActivity.this, "Sorry fam, but the file was not successfully created :(", Toast.LENGTH_LONG).show();
                         }
                         return null;
                     }
@@ -100,39 +157,30 @@ public class WifiActivity extends AppCompatActivity {
         Toast.makeText(WifiActivity.this, text, Toast.LENGTH_SHORT).show();
     }
 
-    public static String executeRemoteCommand(String username, String password, String hostname, int port, String wifiName, String pwString)
+    // Connect to the Pi via SSH using the JSch library
+    public static String executeRemoteCommand(String username, String password, String hostname, int port, String wifiName, String pwString, String security)
             throws Exception{
         JSch j = new JSch();
         Session s = j.getSession(username, hostname, port);
         s.setPassword(password);
-
         Properties p = new Properties();
         p.put("StrictHostKeyChecking", "no");
         s.setConfig(p);
+        s.connect();
 
-        ChannelExec cs = null;
-        try {
-            cs = (ChannelExec) s.openChannel("exec");
-            cs.setCommand("true");
-            cs.connect();
-            //cs.disconnect();
-        } catch (Throwable t) {
-            s = j.getSession(username, hostname, port);
-            s.setPassword(password);
-            s.setConfig(p);
-            s.connect();
-        }
-
-        cs = (ChannelExec) s.openChannel("exec");
+        // Once connected, create a "channel" and execute the required commands
+        ChannelExec cs = (ChannelExec) s.openChannel("exec");
 
         ByteArrayOutputStream b = new ByteArrayOutputStream();
         cs.setOutputStream(b);
         String ssid = "touch \"ssid: " + wifiName + "\"";
         String pass = "touch \"password: " + pwString + "\"";
-        cs.setCommand(ssid + ";" + pass);
+        String sec = "touch \"security: " + security + "\"";
+        cs.setCommand(ssid + ";" + pass + ";" + sec);
         cs.connect();
         Thread.sleep(1000);
         cs.disconnect();
+        s.disconnect();
 
         return b.toString();
     }
